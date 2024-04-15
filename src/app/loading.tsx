7351fd79edfaf57b722daa2bcb9a246c57c4adc1
Priest <Charleschukwuemeka47@gmail.com>
2024-04-15 23:59:53 +0100

    Fix: Made some changes to the side bar and all

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,7 +6,7 @@ import logo from "../../public/Images/logo.svg";
 const loading = () => {
   return (
     <main className="h-screen flex items-center justify-center">
-          <Image src={logo} alt="Logo" className="animate-pulse"/>
+          <Image src={logo} alt="Logo" className="animate-pulse size-14"/>
     </main>
   );
 };
--- src/components/HistoryComponents/History.tsx
+++ src/components/HistoryComponents/History.tsx
@@ -8,7 +8,7 @@ import { Bill, DirectboxReceive, Send2, WalletMoney } from "iconsax-react";
 
 const History = ({transactions, currentCurrency}: any ) => {
     return ( 
-        <main className="border border-[#7676801F] rounded-lg p-4 mb-24 overflow-y-auto special max-h-screen">
+        <main className="border border-[#7676801F] rounded-lg p-4 mb-24 overflow-y-auto special1 max-h-screen">
             <p className="text-sm lg:text-base text-[#06121B] font-semibold">All transactions</p>
             <div className="flex flex-col gap-y-2 mt-6 mb-4">
                 {transactions && transactions.map((transaction: any) => (
